b578888cec5eaef5582087995eba8830409fccf8
Prashanth <imprash4u@gmail.com>
2021-09-30 23:39:45 +0530

    Added: Creating database in Mysql Workbench and connection database in the java program

--- src/main/java/com/bridgelabz/employeepayrollservicejdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservicejdbc/EmployeePayrollService.java
@@ -1,7 +1,28 @@
 package com.bridgelabz.employeepayrollservicejdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollService {
     public static void main(String[] args) {
-        System.out.println("\n\tWelcome to Employee Payroll service Using JDBC");
+        EmployeePayrollService service = new EmployeePayrollService();
+        service.getConnection();
+    }
+
+    private void getConnection() {
+        String url = "jdbc:mysql://127.0.0.1:3306/employeePayroll_service";
+        String username = "root";
+        String password = "rootpassword";
+        Connection connection = null;
+        try {
+
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
