16a94542bc0db241f4b351a47948fdd2ef95ead0
Prashanth <imprash4u@gmail.com>
2021-09-30 23:31:51 +0530

    Added: Welcome message

---
+++ README.md
@@ -0,0 +1,1 @@
+# EmployeePayroll_Service_JDBC
---
+++ src/main/java/com/bridgelabz/employeepayrollservicejdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrollservicejdbc;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("\n\tWelcome to Employee Payroll service Using JDBC");
+    }
+}
